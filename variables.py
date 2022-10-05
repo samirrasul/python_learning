7d3e4371d8730fb8eb60fb659ebde7324054a4a3
samirrasul <samirrasul15oct@gmail.com>
2022-10-05 21:41:04 +0200

    Variables added

---
+++ variables.py
@@ -0,0 +1,30 @@
+'''
+x = str(3)
+y = int(3)
+print(type(x));
+print(y);
+'''
+'''
+x = 'Hello, World'
+y = "Hello, World"
+print(x)
+print(y)
+'''
+'''
+x, y, z = "Hungary", "Azerbaijan", "Turkey"
+print(z)
+'''
+'''
+countries = "Hungary", "Azerbaijan", "Turkey"
+x, y, z = countries
+print(y)
+'''
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
