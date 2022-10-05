df9b8a0fe21eade222315bd6ca3937309fe9cefe
samirrasul <samirrasul15oct@gmail.com>
2022-10-05 23:13:23 +0200

    Strings added

---
+++ numbers.py
@@ -0,0 +1,7 @@
+#x = 12
+#y = '2'
+#print(float(y))
+
+import random
+
+print(random.randrange(1, 10))
---
+++ strings.py
@@ -0,0 +1,40 @@
+'''
+str = """I am writing multiline
+string for
+    sure"""
+
+print(str)
+'''
+
+''' 
+str = 'Hello, World'
+print(str[1])
+'''
+
+'''
+for x in 'banana':
+    print(x)
+
+print(len('banana'))
+'''
+
+'''
+txt = 'I wanna learn Python as soon as possible'
+print('soon' in txt)
+
+if 'soon' in txt:
+    print('"soon" is present in text')
+'''
+
+'''
+txt = 'Hello, World'
+print(txt[-5:-2])
+'''
+txt = '   Samir Rasulov'
+print(txt.upper())
+print(txt.lower())
+print(txt.strip())
+print(txt.replace("Sam", "Das"))
+print(txt.strip().split(' '))
+
+
