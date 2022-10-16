f1a5870d75b1ff8c5b7f6a369c423f5112a61fee
samirrasul <samirrasul15oct@gmail.com>
2022-10-16 23:03:36 +0200

    Sets added

---
+++ sets.py
@@ -0,0 +1,11 @@
+set1 = {"app", "add", "carry"}
+'''
+print(set1)
+print("apple" in set1)
+'''
+'''set2 ={"application", "addition", "carriage"};
+set1.update(set2);
+list1 = ["trabzon"];
+set1.update(list1);
+print(set1)'''
+
