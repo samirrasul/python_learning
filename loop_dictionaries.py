d3621a04c604dfb76877d49fbebbed94b9737779
samirrasul <samirrasul15oct@gmail.com>
2022-10-21 22:00:55 +0200

    Looping dictionaries added

---
+++ loop_dictionaries.py
@@ -0,0 +1,13 @@
+this_dict = {
+    "name": "Jacob",
+    "surname": "Hossain",
+    "age": 32
+}
+
+'print(this_dict)'
+
+'''for x in this_dict: 
+    print(this_dict[x])'''
+
+for x, y in this_dict.items():
+    print(x, ":", y);
