4883b8a66641a55dd44b3ec0e7308232201f1966
samirrasul <samirrasul15oct@gmail.com>
2022-10-21 22:50:31 +0200

    While loop added

---
+++ while_loop.py
@@ -0,0 +1,22 @@
+'''
+i = 1;
+while i < 10:
+    if i > 7.5:
+        break;
+    print(i);
+    i = i + 1;
+'''
+'''
+i = 0;
+while i < 10:
+    i += 1;
+    if i == 5:
+        continue;
+    print(i);
+'''
+i = 0;
+while i < 10:
+    print(i);
+    i += 1;
+else: 
+    print("Mission completed");
